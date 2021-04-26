0a1bc2c8c3fe0691f79486e1afe418757920bd22
mchevez <mauricechevez@gmail.com>
2021-04-26 15:02:37 -0400

    Delete js/capitals.js - No longer needed

--- js/capitals.js
+++
@@ -1,7 +0,0 @@
-
-// Hold all state/territory value here
-let states = [
-    {state:'New York', capital:'Albany'},
-    {state:'New Jersey', capital:'Trenton'},
-    {state:'Main', capital:'Trenton'},
-]
